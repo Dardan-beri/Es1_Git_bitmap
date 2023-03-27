61482debb15c9b4341fc045c48d324c3c6fc9fe8
dardanmat <dardan.berisha@stud.unifi.it>
2023-03-27 17:27:59 +0200

    Added version name.

---
+++ Version.h
@@ -0,0 +1,10 @@
+//
+// Created by Dardan Matias Berisha :) on 27/03/23.
+//
+
+#ifndef GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+#define GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
+
+static const std::string VERSION = "v1.0";
+
+#endif //GIT_EXAMPLE_IMAGE_VIEWER_VERSION_H
